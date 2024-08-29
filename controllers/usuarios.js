148650056e084622c704b77faac436b662f33a02
AlejandroCastroT <henry_acastrot@soy.sena.edu.co>
2024-08-28 22:41:04 -0500

    Implementada y probada la funcionalidad para inhabilitar usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -101,6 +101,30 @@ ruta.put('/:email', async (req, res)  => {
     }
 });
 
+//funcion asincronica para inactivar un usuario
+async function  desactivarUsuario(email){
+    let usuario = await Usuario.findOneAndUpdate({"email": email}, {
+        $set: {
+            estado: false
+        }
+    }, {new: true});
+    return usuario;
+}
+
+// Endpoint de tipo DELETE para el recurso usuarios
+ruta.delete('/:email',(req,res) => {
+    let resultado = desactivarUsuario(req.params.email);
+    resultado.then(valor => {
+        res.json({
+           usuario: valor
+        })
+    }).catch(err => {
+       res.status(400).json({
+        err
+      })
+       
+    });
+});
 
 
 
